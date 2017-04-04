908166fa464332ff8855d019ff11abc1e5abd64f
jeremN <jeremne@gmail.com>
2017-04-04 12:45:18 +0200

    base file

--- Angular 1.X/assets/js/animPanel.js
+++
@@ -1,5 +0,0 @@
-angular.module('ui.bootstrap').controller('CollapseDemoCtrl', function ($scope) {
-  $scope.isNavCollapsed = true;
-  $scope.isCollapsed = false;
-  $scope.isCollapsedHorizontal = false;
-});
